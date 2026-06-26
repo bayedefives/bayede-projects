079cf7171b40061cd24fd2cbff8e840d4ce31230
Bayede Mthethwa <www.bayedemthethwa.zibongele@gmail.com>
2026-06-26 20:23:56 +0200

    README.md
    # Personal Portfolio & Projects
    
    👋 Hi, I’m Bayede Mthethwa  
    🎓 Final-year IT student at Vaal University of Technology  
    📊 Passionate about Data Analytics, Programming, and Problem-Solving  
    
    ---
    
    ## 🚀 Projects
    - **Personal Portfolio Website** (HTML, CSS)  
    - **Student Management System** (VB.NET + SQL)  
    - **Cisco Data Analytics Essentials Coursework** (Excel, SQL, Tableau)  
    - Software I usually use: Visual Studio 2022, VS Code, Oracle FreeSQL  
    
    ---
    
    ## 🛠️ Skills
    - Python | SQL | VB.NET | HTML | PL/SQL | MS Office  
    
    ---
    
    💡 *Always learning, building, and sharing insights through code.*

---
+++ Form1.Designer.vb
@@ -0,0 +1,110 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnHello = New System.Windows.Forms.Button()
+        Me.lblMessage = New System.Windows.Forms.Label()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.lblStatus = New System.Windows.Forms.Label()
+        Me.btnAbout = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnHello
+        '
+        Me.btnHello.Location = New System.Drawing.Point(312, 131)
+        Me.btnHello.Name = "btnHello"
+        Me.btnHello.Size = New System.Drawing.Size(169, 65)
+        Me.btnHello.TabIndex = 0
+        Me.btnHello.Text = "PrintHello"
+        Me.btnHello.UseVisualStyleBackColor = True
+        '
+        'lblMessage
+        '
+        Me.lblMessage.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.lblMessage.Location = New System.Drawing.Point(275, 240)
+        Me.lblMessage.Name = "lblMessage"
+        Me.lblMessage.Size = New System.Drawing.Size(227, 74)
+        Me.lblMessage.TabIndex = 1
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(106, 66)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 32)
+        Me.btnClear.TabIndex = 2
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnClose
+        '
+        Me.btnClose.Location = New System.Drawing.Point(603, 66)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(75, 32)
+        Me.btnClose.TabIndex = 3
+        Me.btnClose.Text = "Close"
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'lblStatus
+        '
+        Me.lblStatus.AutoSize = True
+        Me.lblStatus.Location = New System.Drawing.Point(372, 356)
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(0, 20)
+        Me.lblStatus.TabIndex = 4
+        '
+        'btnAbout
+        '
+        Me.btnAbout.BackColor = System.Drawing.Color.Thistle
+        Me.btnAbout.Location = New System.Drawing.Point(350, 39)
+        Me.btnAbout.Name = "btnAbout"
+        Me.btnAbout.Size = New System.Drawing.Size(75, 36)
+        Me.btnAbout.TabIndex = 5
+        Me.btnAbout.Text = "About"
+        Me.btnAbout.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 418)
+        Me.Controls.Add(Me.btnAbout)
+        Me.Controls.Add(Me.lblStatus)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.lblMessage)
+        Me.Controls.Add(Me.btnHello)
+        Me.Name = "Form1"
+        Me.Text = "u"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnHello As Button
+    Friend WithEvents lblMessage As Label
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnClose As Button
+    Friend WithEvents lblStatus As Label
+    Friend WithEvents btnAbout As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+    Private Sub btnHello_Click(sender As Object, e As EventArgs) Handles btnHello.Click
+        lblMessage.Text = "Hello, World! My name is Bayde Mthethwa" & vbCrLf & "This is my first Visual Basic program."
+    End Sub
+
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        If MessageBox.Show("Are you sure you want to close the program?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question) = DialogResult.Yes Then
+            Me.Close()
+        End If
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        lblMessage.Text = " "
+        lblStatus.Text = "Message cleared."
+    End Sub
+
+    Private Sub btnAbout_Click(sender As Object, e As EventArgs) Handles btnAbout.Click
+        MessageBox.Show("This program was created by Bayde Mthethwa as a simple demonstration of Visual Basic programming.", "About", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+End Class
